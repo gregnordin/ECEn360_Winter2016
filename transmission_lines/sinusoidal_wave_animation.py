a52de5c0cfed481d294bba9fa4216104f8f74b94
gregnordin <nordin@byu.edu>
2016-01-12 19:28:20 -0700

    add sum of forward and reverse waves along with corresponding checkbox

--- transmission_lines/sinusoidal_wave_animation.py
+++ transmission_lines/sinusoidal_wave_animation.py
@@ -16,7 +16,7 @@ freq_Hz = 100e6
 velocity_mps = 2e8
 wavelength_m = velocity_mps / freq_Hz
 twopi = 2.0*np.pi
-n_samp_one_temporal_cycle = 30
+n_samp_one_temporal_cycle = 60
 alpha = 0.0
 reflection_coef = 1.0
 current_time = 0.0
@@ -28,17 +28,21 @@ z = np.linspace(zmin,zmax,numpnts)
 z_norm = z/wavelength_m
 color_forward_wave = 'blue'
 color_reverse_wave = 'red'
+color_sum_wave = 'black'
+alpha_sum_wave = 0.3
 v1 = forward_wave()
 v2 = reverse_wave()
+vsum = v1 + v2
 
 ii = 0
 
 # Set up plot
 p = figure(plot_width=600, plot_height=400, x_range=(zmin,zmax), y_range=(-2.1,2.1), \
            title="Forward & Reverse Sinusoidal Voltages", \
-           tools="pan,box_zoom,resize,save,reset")
+           tools="pan,box_zoom,save,reset")
 l_forward = p.line(z, v1, line_width=2, color=color_forward_wave, line_alpha=0.5)
 l_reverse = p.line(z, v2, line_width=2, color=color_reverse_wave, line_alpha=0.5)
+l_sum = p.line(z, vsum, line_width=2, color=color_sum_wave, line_alpha=0.0)
 p.xaxis.axis_label = "z (m)"
 p.yaxis.axis_label = "Voltage (V)"
 t1 = p.text(zmin+1.5, 1.5, text=['f = {} MHz'.format(freq_Hz/1.e6)],
@@ -69,6 +73,8 @@ def reset_handler():
     current_time = 0
     l_forward.data_source.data["y"] = forward_wave()
     l_reverse.data_source.data["y"] = reverse_wave()
+    l_sum.data_source.data["y"] = l_forward.data_source.data["y"] + \
+                                  l_reverse.data_source.data["y"]
     #t2.data_source.data["text"] = ['t = {:.3f} s'.format(current_time)]
 button_reset = Button(label="Reset", type="success")
 button_reset.on_click(reset_handler)
@@ -77,17 +83,19 @@ button_reset.on_click(reset_handler)
 def checkbox_group_handler(active):
     if 0 in active:
         l_forward.glyph.line_alpha = 0.5
-        #l_forward.glyph.line_color = color_forward_wave
     else:
         l_forward.glyph.line_alpha = 0.0
     if 1 in active:
         l_reverse.glyph.line_alpha = 0.5
-        #l_reverse.glyph.line_color = color_reverse_wave
     else:
         l_reverse.glyph.line_alpha = 0.0
+    if 2 in active:
+        l_sum.glyph.line_alpha = alpha_sum_wave
+    else:
+        l_sum.glyph.line_alpha = 0.0
     #t1.data_source.data["text"] = ['{} {}'.format(l_forward.glyph.line_alpha,l_reverse.glyph.line_alpha)]
 checkbox_group = CheckboxGroup(
-        labels=["Forward Propagating Wave", "Reverse Propagating Wave"], active=[0, 1])
+        labels=["Forward Propagating Wave", "Reverse Propagating Wave", "Sum of Waves"], active=[0, 1])
 checkbox_group.on_click(checkbox_group_handler)
 
 # Set up slider & callback function
@@ -97,6 +105,8 @@ def update_alpha(attrname, old, new):
     if not toggle.active:
         l_forward.data_source.data["y"] = forward_wave()
         l_reverse.data_source.data["y"] = reverse_wave()
+        l_sum.data_source.data["y"] = l_forward.data_source.data["y"] + \
+                                      l_reverse.data_source.data["y"]
 alpha_slider = Slider(title="Attenuation Constant, \u03B1 (1/m)", value=0.0, start=0.0, end=0.25, step=0.005)
 alpha_slider.on_change('value', update_alpha)
 
@@ -107,6 +117,8 @@ def update_gamma(attrname, old, new):
     if not toggle.active:
         l_forward.data_source.data["y"] = forward_wave()
         l_reverse.data_source.data["y"] = reverse_wave()
+        l_sum.data_source.data["y"] = l_forward.data_source.data["y"] + \
+                                      l_reverse.data_source.data["y"]
 gamma_slider = Slider(title="Reflection Coefficient, \u0393", value=reflection_coef, start=-1.0, end=1.0, step=0.01)
 gamma_slider.on_change('value', update_gamma)
 
@@ -121,4 +133,6 @@ def update():
         current_time = ii / n_samp_one_temporal_cycle
         l_forward.data_source.data["y"] = forward_wave()
         l_reverse.data_source.data["y"] = reverse_wave()
+        l_sum.data_source.data["y"] = l_forward.data_source.data["y"] + \
+                                      l_reverse.data_source.data["y"]
         #t2.data_source.data["text"] = ['t = {:.3f} s'.format(current_time)]
